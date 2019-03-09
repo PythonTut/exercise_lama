26711805d4bcd6e26e9479a4828d2c0e1c7390d0
Timo Michel <TimoM96@outlook.de>
2019-03-09 16:57:16 +0100

    Added TODO's

--- lama.py
+++ lama.py
@@ -3,26 +3,31 @@ from typing import List, Tuple
 
 # Changing a single tile
 def change(field: List[List[int]], tile: Tuple[int, int]) -> List[List[int]]:
+    # TODO implement function
     pass
 
 
 # Changing all necessary tiles
 def change_tiles_normal(field: List[List[int]], tile: Tuple[int, int]) -> List[List[int]]:
+    # TODO implement function
     pass
 
 
 # Change tiles with special rules
 def change_tiles_special(field: List[List[int]], tile: Tuple[int, int]) -> List[List[int]]:
+    # TODO implement function
     pass
 
 
 # String-representation for the field
 def construct_str(field: List[List[int]]) -> str:
+    # TODO implement function
     pass
 
 
 # Checks whether the game is won or not
 def check_solved(field: List[List[int]]) -> bool:
+    # TODO implement function
     pass
 
 
